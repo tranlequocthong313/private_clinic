2fce1dbab4f32865f1a99647100630c16bb08d6f
tranlequocthong313 <tranlequocthong313@example.com>
2024-01-05 19:45:24 +0700

    updated email, sms templates

--- app/utils.py
+++ app/utils.py
@@ -2,13 +2,7 @@ import random
 
 
 def random_password():
-    lower = "abcdefghijklmnopqrstuvwxyz"
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    numbers = "0123456789"
-    symbols = "@#$&_-()=%*:/!?+."
-    string = lower + upper + numbers + symbols
-    password = "".join(random.sample(string, 12))
-    return password
+    return "".join(random.choices("0123456789", k=8))
 
 
 def format_money(money):
